24813f54478acb81b956e34fd6e28b2c25ecf84d
Egemen Eliz <egemenelz@gmail.com>
2021-06-02 22:14:26 -0400

    DONE

--- src/test/java/WebstaurantTest.java
+++ src/test/java/WebstaurantTest.java
@@ -11,18 +11,37 @@ public class WebstaurantTest {
     static WebElement nextPageButton;
     static String item;
 
+    /**
+     * OPERATING SYSTEM CONFIGURATION
+     * WEB DRIVER SETUP
+     * THEN NAVIGATE TO APPLICATION' URL
+     */
+
     static {
-        // DRIVER SETUP'S AND NAVIGATE TO APPLICATIONS URL
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.webstaurantstore.com/");
+
+        String OS = System.getProperty("os.name");
+        System.out.println(OS);
+
+        if (OS.equals("Windows 10")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.get("https://www.webstaurantstore.com/");
+
+        } else if (OS.equals("Mac OS X")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.get("https://www.webstaurantstore.com/");
+        }else{
+            System.exit(1);
+        }
 
         item = "stainless work table";
     }
 
     public static void main(String[] args) {
-        // ENTER THE ITEM AND SEARCH
+        // ENTER THE ITEM IN SEARCH ,AND THEN HIT ENTER
         WebElement searchButton = driver.findElement(By.id("searchval"));
         searchButton.sendKeys(item, Keys.ENTER);
 
@@ -46,14 +65,16 @@ public class WebstaurantTest {
         getEmptyCart();
         verifyCartEmpty();
 
-        // END OF THE AUTOMATION CLOSE THE BROWSER SESSION
+        /**
+         * END OF THE AUTOMATION CLOSES THE BROWSER SESSION
+         */
         driver.close();
     }
 
 
     /**
      * GETS ALL THE ITEM IN THE RESULT AND ADD INTO LIST
-     * VERIFIES IS EVERY ITEM CONTAINS "Table" OR NOT
+     * VERIFIES IF EVERY ITEM CONTAINS "Table" OR NOT
      */
     public static void allTextOfItems() {
         try {
@@ -77,8 +98,8 @@ public class WebstaurantTest {
     }
 
     /**
-     * SAVE'S INTO LIST EVERY "Add To Cart" BUTTON
-     * CLICK TO THE LAST ITEM
+     * SAVES EVERY "Add To Cart" BUTTON INTO LIST OF WEBELEMENT
+     * AND THEN, CLICKS THE LAST BUTTON
      */
     public static void addToCart() {
         List<WebElement> addToCart = driver.findElements(By.xpath("//input[starts-with(@name,\"addToCartButton\")]"));
@@ -86,8 +107,7 @@ public class WebstaurantTest {
     }
 
     /**
-     * EXPLICIT WAIT FOR CLICK TO ELEMENT
-     *
+     * EXPLICIT WAIT FOR CLICK TO WEBELEMENT SAFELY
      * @param webElement
      */
     public static void clickTo(WebElement webElement) {
@@ -96,8 +116,8 @@ public class WebstaurantTest {
     }
 
     /**
-     * CLICK TO CART BUTTON ON THE POP-UP AFTER WE CLICK ADD TO CART
-     * CLICK TO EMPTY CART AFTER WE NAVIGATE TO CART PAGE
+     * CLICKS CART BUTTON ON THE POP-UP AFTER WE CLICK "Add to Cart" BUTTON
+     * AFTER THAT CLICKS "Empty Cart" BUTTON AFTER WE NAVIGATE TO "Cart" PAGE
      * AND ACCEPTS EMPTY CART ALERT
      */
     public static void getEmptyCart() {
@@ -110,7 +130,7 @@ public class WebstaurantTest {
     }
 
     /**
-     * SCROLLS ALL THE WAY DOWN FOR GET FULL LIST OF ITEMS
+     * SCROLLS ALL THE WAY DOWN TO GET FULL LIST OF ITEMS
      */
     public static void scrollingDown() {
         JavascriptExecutor js = (JavascriptExecutor) driver;
@@ -119,7 +139,7 @@ public class WebstaurantTest {
 
     /**
      * I USED EXPLICIT HERE BECAUSE SOMETIMES SELENIUM WORKS FASTER THAN APPLICATION
-     * AND IT DOESN'T VERIFIES CART SUCCESSFULLY EMPTIED
+     * AND IT DOESN'T VERIFIES IF CART IS SUCCESSFULLY EMPTIED
      */
     public static void verifyCartEmpty() {
         WebDriverWait wait = new WebDriverWait(driver, 10);
